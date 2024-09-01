388fcf5d8dbc46ad35fbd383b071be1103ea64d8
FES001 <firdavs7794948@gmail.com>
2024-09-02 02:00:01 +0500

    Footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const video = document.querySelectorAll('.video');
 const backgroundVideoItself = document.querySelectorAll('.video-background-itself');
+const client = document.querySelectorAll('.client');
+const clientVideo = document.querySelectorAll('.client-video-background-itself');
 
 // VIDEOS
 
@@ -10,4 +12,14 @@ for (let i = 0; i < video.length; i++) {
     video[i].addEventListener('mouseleave', () => {
         backgroundVideoItself[i].pause();
     });
+
+    // MY CLIENTS
+
+    client[i].addEventListener('mouseenter', () => {
+        clientVideo[i].play();
+    });
+    client[i].addEventListener('mouseleave', () => {
+        clientVideo[i].pause();
+    });
+
 };
