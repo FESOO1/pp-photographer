ad7f6da9c3a6e0d4990dba88aaa4a3b0eeb3b4b8
FES001 <firdavs7794948@gmail.com>
2024-09-03 02:45:21 +0500

    Videos page is done.

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,24 +1,24 @@
-const photosControllerButtons = document.querySelectorAll('.photos-controller-button');
-const photosContainer = document.querySelector('.photos-container');
+const videosControllerButtons = document.querySelectorAll('.videos-controller-button');
+const videosContainer = document.querySelector('.videos-container');
 const fullViewBtn = document.getElementById('fullViewBtn');
 const gridViewBtn = document.getElementById('gridViewBtn');
-const photo = document.querySelectorAll('.photo');
+const video = document.querySelectorAll('.videos-itself');
 const customCursor = document.querySelector('.view-cursor');
 
 fullViewBtn.addEventListener('click', () => {
 
     // REMOVE THE CLASS FROM ALL THE BUTTONS
-    for (const controllerButtons of photosControllerButtons) {
-        controllerButtons.classList.remove('active-photos-button');
+    for (const controllerButtons of videosControllerButtons) {
+        controllerButtons.classList.remove('active-videos-button');
     };
 
-    // ADDING THE CLASS TO THE CLICKED BUTTON
+    // ADDING THE CLASS TO THE CLICKED BUTTONvideos
 
-    fullViewBtn.classList.add('active-photos-button');
+    fullViewBtn.classList.add('active-videos-button');
 
     // MAKING THE FULL VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
 
-    photosContainer.classList.remove('photos-container-js');
+    videosContainer.classList.remove('videos-container-js');
 
     // MAKING THE CUSTOM CURSOR VISIBLE 
 
@@ -29,17 +29,17 @@ gridViewBtn.addEventListener('click', () => {
 
     // REMOVE THE CLASS FROM ALL THE BUTTONS
 
-    for (const controllerButtons of photosControllerButtons) {
-        controllerButtons.classList.remove('active-photos-button');
+    for (const controllerButtons of videosControllerButtons) {
+        controllerButtons.classList.remove('active-videos-button');
     };
 
     // ADDING THE CLASS TO THE CLICKED BUTTON
 
-    gridViewBtn.classList.add('active-photos-button');
+    gridViewBtn.classList.add('active-videos-button');
 
     // MAKING THE GRID VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
 
-    photosContainer.classList.add('photos-container-js');
+    videosContainer.classList.add('videos-container-js');
 
     // MAKING THE CUSTOM CURSOR INVISIBLE 
 
@@ -55,18 +55,15 @@ function viewCursor(e) {
     const positionY = e.clientY - 15;
     const positionX = e.clientX - 35;
 
-
     customCursor.style.transform = `translateX(${positionX}px) translateY(${positionY}px)`;
-   /*  customCursor.style.top = `${positionY}px`;
-    customCursor.style.left = `${positionX}px`; */
 };
 
 
-for (let i = 0; i < photo.length; i++) {
-    photo[i].addEventListener('mouseenter', () => {
+for (let i = 0; i < video.length; i++) {
+    video[i].addEventListener('mouseenter', () => {
         customCursor.style.opacity = '1';
     });
-    photo[i].addEventListener('mouseleave', () => {
+    video[i].addEventListener('mouseleave', () => {
         customCursor.style.opacity = '0';
     });
 };
