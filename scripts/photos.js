89116dc75d965f65af2840d25caa10b93b884024
FES001 <firdavs7794948@gmail.com>
2024-09-03 01:32:51 +0500

    The photos page is almost done.

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,21 +1,38 @@
 const photosControllerButtons = document.querySelectorAll('.photos-controller-button');
 const photosContainer = document.querySelector('.photos-container');
 const fullViewBtn = document.getElementById('fullViewBtn');
-
-for (let i = 0; i < photosControllerButtons.length; i++) {
-    photosControllerButtons[i].addEventListener('click', () => {
-        // REMOVE THE CLASS FROM ALL THE BUTTONS
-
-        for (const controllerButtons of photosControllerButtons) {
-            controllerButtons.classList.remove('active-photos-button');
-        };
-
-        // ADD THE CLASS TO THE CLICKED BUTTON
-
-        photosControllerButtons[i].classList.add('active-photos-button');
+const gridViewBtn = document.getElementById('gridViewBtn');
 
 
-        photosContainer.classList.add('photos-container-js');
+fullViewBtn.addEventListener('click', () => {
 
-    });
-};
+    // REMOVE THE CLASS FROM ALL THE BUTTONS
+    for (const controllerButtons of photosControllerButtons) {
+        controllerButtons.classList.remove('active-photos-button');
+    };
+
+    // ADDING THE CLASS TO THE CLICKED BUTTON
+
+    fullViewBtn.classList.add('active-photos-button');
+
+    // MAKING THE FULL VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
+
+    photosContainer.classList.remove('photos-container-js');
+});
+
+gridViewBtn.addEventListener('click', () => {
+
+    // REMOVE THE CLASS FROM ALL THE BUTTONS
+
+    for (const controllerButtons of photosControllerButtons) {
+        controllerButtons.classList.remove('active-photos-button');
+    };
+
+    // ADDING THE CLASS TO THE CLICKED BUTTON
+
+    gridViewBtn.classList.add('active-photos-button');
+
+    // MAKING THE GRID VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
+
+    photosContainer.classList.add('photos-container-js');
+});
