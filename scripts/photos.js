2cd59d63a40607567cf7f639fdc9c6162def9df5
FES001 <firdavs7794948@gmail.com>
2024-09-03 01:52:09 +0500

    Almost done.

--- scripts/photos.js
+++ scripts/photos.js
@@ -2,7 +2,8 @@ const photosControllerButtons = document.querySelectorAll('.photos-controller-bu
 const photosContainer = document.querySelector('.photos-container');
 const fullViewBtn = document.getElementById('fullViewBtn');
 const gridViewBtn = document.getElementById('gridViewBtn');
-
+const photo = document.querySelectorAll('.photo');
+const customCursor = document.querySelector('.view-cursor');
 
 fullViewBtn.addEventListener('click', () => {
 
@@ -18,6 +19,10 @@ fullViewBtn.addEventListener('click', () => {
     // MAKING THE FULL VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
 
     photosContainer.classList.remove('photos-container-js');
+
+    // MAKING THE CUSTOM CURSOR VISIBLE 
+
+    customCursor.style.display = 'flex';
 });
 
 gridViewBtn.addEventListener('click', () => {
@@ -35,4 +40,33 @@ gridViewBtn.addEventListener('click', () => {
     // MAKING THE GRID VIEW WORK THROUGH ADDING CLASS TO THE PARENT ELEMENT
 
     photosContainer.classList.add('photos-container-js');
+
+    // MAKING THE CUSTOM CURSOR INVISIBLE 
+
+    customCursor.style.display = 'none';
 });
+
+// CURSOR
+
+window.addEventListener('mousemove', viewCursor);
+
+function viewCursor(e) {
+
+    const positionY = e.clientY - 15;
+    const positionX = e.clientX - 35;
+
+
+    customCursor.style.transform = `translateX(${positionX}px) translateY(${positionY}px)`;
+   /*  customCursor.style.top = `${positionY}px`;
+    customCursor.style.left = `${positionX}px`; */
+};
+
+
+for (let i = 0; i < photo.length; i++) {
+    photo[i].addEventListener('mouseenter', () => {
+        customCursor.style.opacity = '1';
+    });
+    photo[i].addEventListener('mouseleave', () => {
+        customCursor.style.opacity = '0';
+    });
+};
