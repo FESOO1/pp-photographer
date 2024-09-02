db369b4e104099a5c9dd856554916b28e20c39d2
FES001 <firdavs7794948@gmail.com>
2024-09-03 00:47:43 +0500

    s

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,18 +1,21 @@
 const photosControllerButtons = document.querySelectorAll('.photos-controller-button');
+const photosContainer = document.querySelector('.photos-container');
+const fullViewBtn = document.getElementById('fullViewBtn');
 
-photosControllerButtons.forEach(controllerButton => {
-    controllerButton.addEventListener('click', () => {
-
+for (let i = 0; i < photosControllerButtons.length; i++) {
+    photosControllerButtons[i].addEventListener('click', () => {
         // REMOVE THE CLASS FROM ALL THE BUTTONS
 
         for (const controllerButtons of photosControllerButtons) {
             controllerButtons.classList.remove('active-photos-button');
         };
 
-
         // ADD THE CLASS TO THE CLICKED BUTTON
 
-        controllerButton.classList.add('active-photos-button');
+        photosControllerButtons[i].classList.add('active-photos-button');
+
+
+        photosContainer.classList.add('photos-container-js');
 
     });
-});
+};
