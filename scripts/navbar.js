a0163d8b18abc00baa500c65bce33ba3ed8ea03e
FES001 <firdavs7794948@gmail.com>
2024-09-06 03:55:03 +0500

    Navbar is fixed.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,9 @@
 const menuBtn = document.querySelector('.menu');
 const nav = document.querySelector('nav');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('menu-btn-js');
     nav.classList.toggle('navbar-js');
+    body.classList.toggle('body-js');
 });
