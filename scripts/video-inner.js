340936b1db181c33f52dd183576578d75bd8eb3b
FES001 <firdavs7794948@gmail.com>
2024-09-06 22:24:27 +0500

    Almost done.

--- scripts/video-inner.js
+++ scripts/video-inner.js
@@ -11,3 +11,23 @@ for (let i = 0; i < video.length; i++) {
         backgroundVideoItself[i].pause();
     });
 };
+
+// 
+
+function setAutoPlay() {
+    const videos = document.querySelectorAll('video');
+
+    for (const video of videos) {
+        if (window.innerWidth < 1200) {
+            video.setAttribute('autoplay', '');
+            video.play();
+        } else {
+            video.removeAttribute('autoplay', '');
+            video.pause();
+        };
+    };
+};
+
+setAutoPlay();
+
+window.addEventListener('resize', setAutoPlay);
